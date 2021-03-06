1473789ef9c0cc6fd0b76f5f400463246554c1db
Erin <westonerin1@gmail.com>
2021-03-06 05:28:42 -0500

    add dynamic type and experience to pokecard component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -7,6 +7,8 @@ class Pokecard extends Component {
     return <div className="Pokecard">
       <h1>{this.props.name}</h1>
       <img src={imgSrc} alt={this.props.name} />
+      <div>Type: {this.props.type}</div>
+      <div>EXP: {this.props.exp}</div>
     </div>
   }
 }
